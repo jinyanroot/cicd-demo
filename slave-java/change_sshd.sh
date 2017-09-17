c3847020c903a36cb9550161e167ae93a6a56338
lijinyan <lijinyan@zallds.com>
2017-09-18 00:49:14 +0800

    先改密码再重启sshd

--- slave-java/change_sshd.sh
+++ slave-java/change_sshd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s/^PermitRootLogin.*/PermitRootLogin yes/" /etc/ssh/sshd_config && /etc/init.d/ssh restart
 echo root:root@123 | chpasswd
+sed -i "s/^PermitRootLogin.*/PermitRootLogin yes/" /etc/ssh/sshd_config && /etc/init.d/ssh restart
