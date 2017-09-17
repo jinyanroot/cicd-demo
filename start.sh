db8fae7107ad017093095bcfe84f21f9f0783d8b
lijinyan <lijinyan@zallds.com>
2017-09-17 23:40:25 +0800

    add change_sshd.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 # -----------------------------------------------------------------------------
 # Filename:      start.sh
 # Encoding:      UTF-8
@@ -14,20 +14,25 @@
 docker run -d \
        --name slave-java \
        --privileged=true \
-	   --restart=always \
-	   -v /var/run/docker.sock:/var/run/docker.sock \
-	   registry.aliyuncs.com/acs-sample/jenkins-slave-dind-java
+       --restart=always \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       registry.aliyuncs.com/acs-sample/jenkins-slave-dind-java
 if [[ $? -ne 0 ]];then echo 'Start Slave Faild'; exit 1; fi
+sleep 3
+
+# Permit root login slave-java
+docker cp `pwd`/slave-java/change_sshd.sh slave-java:/root
+docker exec slave-java "/root/change_sshd.sh"
 
 # Start Jenkins
 docker run -d -u root \
        --name jenkins \
        --privileged=true \
-	   --restart=always \
-	   --link=slave-java \
+       --restart=always \
+       --link=slave-java \
        -p 8081:8080 \
-	   -p 50000:50000 \
-	   -v /var/run/docker.sock:/var/run/docker.sock \
-	   -v $(which docker):/bin/docker \
-	   -v ./jenkins_home:/var/jenkins_home \
-	   registry.aliyuncs.com/acs-sample/jenkins:2.19.2
+       -p 50000:50000 \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       -v $(which docker):/bin/docker \
+       -v `pwd`/jenkins_home:/var/jenkins_home \
+       registry.aliyuncs.com/acs-sample/jenkins:2.19.2
